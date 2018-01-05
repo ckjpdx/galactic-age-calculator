c6e2b091be9c2d38b80a75e71c2b6536df74755d
Chris Knight Johnson <cknightjohnson@gmail.com>
2018-01-05 15:54:35 -0800

    add calculator logic

--- js/age-calculator-interface.js
+++ js/age-calculator-interface.js
@@ -1,12 +1,22 @@
-import { getAgeSeconds } from './../js/age-calculator.js';
+import { getAgeSeconds, agesOnOtherPlanets } from './../js/age-calculator.js';
+
 $(document).ready(function(){
-  let userAge = '1-1-2016';
-  $('button#submit-bday').click(function(){
-    let userAge = $('#input-bday').val();
-    if (userAge) {
-      let userAgeSeconds = getAgeSeconds(userAge);
-      if (!isNaN(userAgeSeconds)) {
-        $('#age-in-seconds span').text(userAgeSeconds);
+  let ageObj = {};
+  $('button#submit-info').click(function(){
+    ageObj.userBday = $('#input-bday').val();
+    ageObj.lifeExpectancy = $('#input-life-expectancy').val();
+    console.log(ageObj.userBday);console.log(ageObj.lifeExpectancy);
+    if (ageObj.userBday && ageObj.lifeExpectancy) {
+      ageObj.userAgeSeconds = getAgeSeconds(ageObj);
+      console.log(ageObj.userAgeSeconds);
+      if (!isNaN(ageObj.userAgeSeconds) && ageObj.userAgeSeconds > 0) {
+        $('#age-in-seconds span').text(ageObj.userAgeSeconds);
+        ageObj = agesOnOtherPlanets(ageObj);
+        console.log(ageObj.ageArray);console.log(ageObj.lifeRemainingArray);
+        for (let i = 0; i < 5; i++) {
+          console.log(i);console.log(ageObj.ageArray[i]);
+          $('#planet-age-' + i).text(ageObj.ageArray[i]);
+        }
       } else {
         alert('Enter your bday in the correct format');
       }
--- js/age-calculator.js
+++ js/age-calculator.js
@@ -1,3 +1,19 @@
-export function getAgeSeconds(userAge){
-  return moment().diff(userAge, 'seconds');
+export function getAgeSeconds(ageObj){
+  return moment().diff(ageObj.userBday, 'seconds');
+}
+export function agesOnOtherPlanets(ageObj){
+  const earthYearSeconds = 31557600;
+  let ageInEarthYears = ageObj.userAgeSeconds / earthYearSeconds;
+  let ageInMercuryYears = ageInEarthYears * 0.24;
+  let ageInVenusYears = ageInEarthYears * 0.62;
+  let ageInMarsYears = ageInEarthYears * 1.88;
+  let ageInJupiterYears = ageInEarthYears * 11.86;
+  ageObj.ageArray = [ageInEarthYears, ageInMercuryYears, ageInVenusYears, ageInMarsYears, ageInJupiterYears];
+  let lifeRemainingEarth = ageObj.lifeExpectancy - ageInEarthYears;
+  let lifeRemainingMercury = ageObj.lifeExpectancy * 0.24 - ageInMercuryYears;
+  let lifeRemainingVenus = ageObj.lifeExpectancy * 0.62 - ageInVenusYears;
+  let lifeRemainingMars = ageObj.lifeExpectancy * 1.88 - ageInMarsYears;
+  let lifeRemainingJupiter = ageObj.lifeExpectancy * 11.86 - ageInJupiterYears;
+  ageObj.lifeRemainingArray = [lifeRemainingEarth, lifeRemainingMercury, lifeRemainingVenus, lifeRemainingMars, lifeRemainingJupiter];
+  return ageObj;
 }
