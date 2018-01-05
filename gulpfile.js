2faa8f91e81191da29c08933a32872452fde220e
Chris Knight Johnson <cknightjohnson@gmail.com>
2018-01-05 09:27:23 -0800

    add build dependencies and generate page with concatenated sass and js.

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,26 @@ var gulp = require('gulp');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 var utilities = require('gulp-util');
 var del = require('del');
+var babelify = require('babelify');
 var buildProduction = utilities.env.production;
+var lib = require('bower-files')({
+  "overrides":{
+    "bootstrap" : {
+      "main": [
+        "less/bootstrap.less",
+        "dist/css/bootstrap.css",
+        "dist/js/bootstrap.js"
+      ]
+    }
+  }
+});
 
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('concatInterface', function() {
   return gulp.src(['./js/*-interface.js'])
