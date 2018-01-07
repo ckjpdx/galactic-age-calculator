08569fdb53cda7b3d689586e562e930a28e2658b
Chris Knight Johnson <cknightjohnson@gmail.com>
2018-01-06 17:21:38 -0800

    add first test spec

--- spec/calculator-spec.js
+++ spec/calculator-spec.js
@@ -1,9 +1,12 @@
+import { AgeObj, runTheNumbers } from './../js/age-calculator.js';
+
 describe('test user input', function() {
 
   it('should return a message appropriate to the user input for bday', function() {
-    var userBday = '';
-    var lifeExpectancy = 80;
-    var ageObj = new AgeObj(userBday, lifeExpectancy);
+    let userBday = '';
+    let lifeExpectancy = 80;
+    let ageObj = new AgeObj(userBday, lifeExpectancy);
+    runTheNumbers(ageObj);
     expect(ageObj.alertMsg).toEqual('Enter your bday first');
   });
 });
